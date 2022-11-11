6c2e7b726b9b15287ec5ca30ba529ca2a8539c02
GiovanniRaseraF <giovanni.raserafarra@gmail.com>
2022-11-11 15:27:56 +0100

    src: adding debug print

--- src/test.hpp
+++ src/test.hpp
@@ -20,7 +20,7 @@
         std::cerr << std::setw(15) << std::left << " -expected:" << expected << std::endl;\
         std::cerr << std::setw(15) << std::left << " -got:" << func << std::endl;\
         std::cerr << std::noboolalpha;\
-        std::cerr << std::setw(std::strlen(error) + 7) << std::setfill('-') << "" << std::endl;\
+       	std::cerr << std::setw(std::strlen(error) + 7) << std::setfill('-') << "" << std::endl;std::cerr << std::setfill(' ');\
     }\
     else{\
         std::cerr << std::setw(std::strlen(error) + 8) << std::setfill('-') << "" << std::endl;\
@@ -30,9 +30,40 @@
 }
 #define CHRONO_TEST(func, message)\
 {\
+    std::cout << std::setw(std::strlen(message) + 9) << std::setfill('-') << "" << std::endl;std::cout << std::setfill(' ');\
+    std::cout << "CHRONING: " << message << std::endl;\
     std::chrono::time_point<std::chrono::system_clock> start, end;\
     start = std::chrono::system_clock::now();\
     func;\
     end = std::chrono::system_clock::now();\
-    std::cout << "Duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() << "ms" << std::endl;\
+    std::cout << std::setw(15) << std::left << " -Duration: " << std::chrono::duration_cast<std::chrono::microseconds>(end-start).count() << "ms" << std::endl;\
+    std::cout << std::setw(std::strlen(message) + 9) << std::setfill('-') << "" << std::endl;std::cout << std::setfill(' ');\
 }
+
+#ifdef DEB
+#define PRINT(str) debugPrint(str);
+#define PRINTLN(str) debugPrintln(str);
+
+void debugPrint(const char *const str){
+    std::cout << "DEB:" << str;
+}
+void debugPrint(std::string str){
+    std::cout << "DEB:" << str;
+}
+void debugPrintln(const char *const str){
+    std::cout << "DEB:" << str << std::endl;
+}
+void debugPrint(double str){
+    std::cout << "DEB:" << str;
+}
+void debugPrintln(double str){
+    std::cout << "DEB:" << str << std::endl;
+}
+void debugPrintln(std::string str){
+    std::cout << "DEB:" << str << std::endl;
+}
+
+#else
+#define PRINT(str) ;
+#define PRINTLN(str) ;
+#endif
